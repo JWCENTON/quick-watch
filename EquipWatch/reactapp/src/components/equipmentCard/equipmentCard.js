438a5f87dcaefb3b632c281af9e26c017f001e41
Ostin-Oskarose <oskarcraft@gmail.com>
2023-06-13 11:47:29 +0200

    Added arguments to equipment card

--- EquipWatch/reactapp/src/components/equipmentCard/equipmentCard.js
+++ EquipWatch/reactapp/src/components/equipmentCard/equipmentCard.js
@@ -2,25 +2,25 @@ import React from 'react';
 import { Card } from 'react-bootstrap';
 import './equipmentCard.css';
 
-function EquipmentCard() {
+function EquipmentCard({ id, name, status, location }) {
     return (
         <>
             <Card style={{ width: "100%", justifyContent: "space-evenly", display: "flex", flexDirection: "row" }}>
                 <div className='cardColumn'>
                     <Card.Title>ID</Card.Title>
-                    <Card.Text>{12345}</Card.Text>
+                    <Card.Text>{id}</Card.Text>
                 </div>
                 <div className='cardColumn'>
                     <Card.Title>Name</Card.Title>
-                    <Card.Text>John Doe</Card.Text>
+                    <Card.Text>{name}</Card.Text>
                 </div>
                 <div className='cardColumn'>
                     <Card.Title>Status</Card.Title>
-                    <Card.Text>In use</Card.Text>
+                    <Card.Text>{status}</Card.Text>
                 </div>
                 <div className='cardColumn'>
                     <Card.Title>Location</Card.Title>
-                    <Card.Text>Warehouse</Card.Text>
+                    <Card.Text>{location}</Card.Text>
                 </div>
             </Card>
         </>
