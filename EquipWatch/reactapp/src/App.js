9b52c40ec883223fbb730b0f884f4db61c81f105
Jacek Walaszek <jwalaszek@centonit.com>
2023-06-13 13:53:20 +0200

    Create company component

--- EquipWatch/reactapp/src/App.js
+++ EquipWatch/reactapp/src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import IndexCompanyView from "./components/mainPage/IndexWithCompanyView";
+import IndexCompanyView from './components/mainPage/IndexWithCompanyView';
 import IndexEmployeeView from './components/mainPage/IndexWithEmployeeView';
+import CompanyCard from './components/companyCard/CompanyCard';
 
 export default function App() {
     return (
-        <div>Hello</div>
+        //<div>Hello</div>
+        <>
+            <CompanyCard name="TestName" address="Warsaw" createdAt="12.07.2022"/>
+            <br/>
+            <CompanyCard name="TestName" address="Warsaw" createdAt="13.07.2022" />
+            <br />
+            <CompanyCard name="TestName" address="Warsaw" createdAt="14.07.2022" />
+            <br />
+            <CompanyCard name="TestName" address="Warsaw" createdAt="15.07.2022" />
+            <br />
+            <IndexCompanyView />
+            <br />
+            <br />
+            <IndexEmployeeView />
+        </>
+
     );
 }
