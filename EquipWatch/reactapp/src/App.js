9563d9c40af0cf9b2aacee91ca54afa387aaf236
Jacek Walaszek <jwalaszek@centonit.com>
2023-06-13 11:15:12 +0200

    Add Main components with css styling

--- EquipWatch/reactapp/src/App.js
+++ EquipWatch/reactapp/src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import IndexCompanyView from "./components/Main/IndexWithCompanyView";
+import IndexEmployeeView from './components/Main/IndexWithEmployeeView';
 
 export default function App() { 
-    return(
-        <div>Hello</div>
+    return (
+        <>
+            <IndexCompanyView />
+            <br/>
+            <IndexEmployeeView />
+        </>
+        
     );
 }
