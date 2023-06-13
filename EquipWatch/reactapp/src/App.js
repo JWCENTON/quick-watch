9804e8d1cffd592c41335f4bdda7c92deae5ec61
Jacek Walaszek <jwalaszek@centonit.com>
2023-06-13 13:02:35 +0200

    Rename Main folder in components

--- EquipWatch/reactapp/src/App.js
+++ EquipWatch/reactapp/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import IndexCompanyView from "./components/Main/IndexWithCompanyView";
-import IndexEmployeeView from './components/Main/IndexWithEmployeeView';
+import IndexCompanyView from "./components/mainPage/IndexWithCompanyView";
+import IndexEmployeeView from './components/mainPage/IndexWithEmployeeView';
 
 export default function App() {
     return (
