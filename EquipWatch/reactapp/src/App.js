0e256457299db9e149f0d06539eba65be0d81f21
dapie346 <dawid.pietruszewski@gmail.com>
2023-06-15 14:16:57 +0200

    changed folder names

--- EquipWatch/reactapp/src/App.js
+++ EquipWatch/reactapp/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import EmployeeMainPage from './pages/EmployeeMain/EmployeeMain';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import CompanyMainPage from './pages/CompanyMain/CompanyMain';
+import CompanyMainPage from './pages/companyMain/CompanyMain';
+import EmployeeMainPage from './pages/employeeMain/EmployeeMain';
 import './App.css';
 
 function App() {
