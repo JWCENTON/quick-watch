cade6f9d0bafe66ef1d09631c5201ca9338aa8b9
Jacek Walaszek <jwalaszek@centonit.com>
2023-06-17 00:48:54 +0200

    Add error page to display when route is not found

--- EquipWatch/reactapp/src/index.js
+++ EquipWatch/reactapp/src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import './index.css';
+import ErrorPage from './pages/error/error-page';
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        errorElement: <ErrorPage />
+    }
+]);
 
 const root = document.getElementById('root');
 createRoot(root).render(
     <React.StrictMode>
-        <App />
+        <RouterProvider router={router} />
     </React.StrictMode>
 );
