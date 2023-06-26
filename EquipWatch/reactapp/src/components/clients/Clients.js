d0058fd91eae9cae5fb236da6c76d9e0a1fc2329
Ostin-Oskarose <oskarcraft@gmail.com>
2023-06-26 11:06:09 +0200

    Client cards display on page

--- EquipWatch/reactapp/src/components/clients/Clients.js
+++ EquipWatch/reactapp/src/components/clients/Clients.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Clients.css';
+import ClientCard from '../clientCard/ClientCard';
 
 function Client() {
     return (
         <div>
             <a className="myAndAllSwitch" href="/" >My clients</a> | <a className="myAndAllSwitch" href="/" >All clients</a>
             <div className="clientContainer">
+                <ClientCard ></ClientCard>
             </div>
         </div>
     );
