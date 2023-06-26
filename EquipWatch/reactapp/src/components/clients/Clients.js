b9028deac6e06e2eb1074c3da2612a234e86a67a
Ostin-Oskarose <oskarcraft@gmail.com>
2023-06-26 11:32:54 +0200

    Implemented layout for client cards

--- EquipWatch/reactapp/src/components/clients/Clients.js
+++ EquipWatch/reactapp/src/components/clients/Clients.js
@@ -4,9 +4,19 @@ import ClientCard from '../clientCard/ClientCard';
 
 function Client() {
     return (
-        <div>
+        <div className="clientSection">
             <a className="myAndAllSwitch" href="/" >My clients</a> | <a className="myAndAllSwitch" href="/" >All clients</a>
             <div className="clientContainer">
+                <ClientCard ></ClientCard>
+                <ClientCard ></ClientCard>
+                <ClientCard ></ClientCard>
+                <ClientCard ></ClientCard>
+                <ClientCard ></ClientCard>
+                <ClientCard ></ClientCard>
+                <ClientCard ></ClientCard>
+                <ClientCard ></ClientCard>
+                <ClientCard ></ClientCard>
+                <ClientCard ></ClientCard>
                 <ClientCard ></ClientCard>
             </div>
         </div>
