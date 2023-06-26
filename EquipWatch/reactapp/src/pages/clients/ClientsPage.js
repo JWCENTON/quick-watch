48b8a782d9e5abffe6f96e90d7bb1bb7bd0d4f55
Marcin Stodolny <marcin.stodolny@o2.pl>
2023-06-26 10:00:24 +0200

    rename file name in ClientsPage

--- EquipWatch/reactapp/src/pages/clients/ClientsPage.js
+++ EquipWatch/reactapp/src/pages/clients/ClientsPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Navigation from '../../components/navigation/Navigation';
 import Sidebar from '../../components/sidebar/Sidebar';
-import Clients from '../../components/clients/clients';
+import Clients from '../../components/clients/Clients';
 
 const ClientsPage = () => {
     return (
