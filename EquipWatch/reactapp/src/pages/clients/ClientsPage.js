ee1dc1a0929e728e4b68c89f88a134a160a5900c
Marcin Stodolny <marcin.stodolny@o2.pl>
2023-06-26 09:57:23 +0200

    fixing Client folder name and file name problem

--- EquipWatch/reactapp/src/pages/clients/ClientsPage.js
+++ EquipWatch/reactapp/src/pages/clients/ClientsPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Navigation from '../../components/navigation/Navigation';
 import Sidebar from '../../components/sidebar/Sidebar';
-import Clients from '../../components/clients/Clients';
+import Clients from '../../components/Clients/clients';
 
 const ClientsPage = () => {
     return (
