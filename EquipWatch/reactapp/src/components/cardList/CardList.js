6e947066f1783de0325d6abd33e91cf93dc135fa
Ostin-Oskarose <oskarcraft@gmail.com>
2023-06-27 13:30:34 +0200

    Page correctly requests data

--- EquipWatch/reactapp/src/components/cardList/CardList.js
+++ EquipWatch/reactapp/src/components/cardList/CardList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState , useEffect} from 'react';
 import { useLocation } from 'react-router-dom';
 import EquipmentCard from '../cards/EquipmentCard';
 import CompanyCard from '../cards/CompanyCard';
@@ -7,38 +7,50 @@ import CommissionCard from '../cards/CommissionCard';
 import './CardList.css';
 
 function CardList() {
+    const [cards, setCards] = useState(null);
     const location = useLocation();
     let itemType;
     let CardComponent;
 
-    switch (location.pathname) {
-    case '/equipment':
-        itemType = 'equipment';
-        CardComponent = EquipmentCard;
-        break;
-    case '/commissions':
-        itemType = 'commissions';
-        CardComponent = CommissionCard;
-        break;
-    case '/clients':
-        itemType = 'clients';
-        CardComponent = ClientCard;
-        break;
-    case '/companies':
-        itemType = 'companies';
-        CardComponent = CompanyCard;
-        break;
-    }
+    useEffect(() => {
+        // eslint-disable-next-line default-case
+        switch (location.pathname) {
+            case '/equipment':
+                itemType = 'equipment';
+                CardComponent = EquipmentCard;
+                break;
+            case '/commissions':
+                itemType = 'commissions';
+                CardComponent = CommissionCard;
+                break;
+            case '/clients':
+                itemType = 'clients';
+                CardComponent = ClientCard;
+                GetClientData();
+                break;
+            case '/companies':
+                itemType = 'companies';
+                CardComponent = CompanyCard;
+                break;
+        }
+    }, []);
 
     return (
         <div className="cardSection">
             <a className="myAndAllSwitch" href="/" >My {itemType}</a> | <a className="myAndAllSwitch" href="/" >All {itemType}</a>
             <div className="cardsContainer">
-                <CardComponent id="123" name="Placeholder Name" address="Placeholder Address" clientName="Placeholder Client Name" status="Placeholder Status" location="Placeholder Location" phone="Placeholder Phone" createdAt="Placeholder Date" recentCommission="Placeholder Commission" />
-                <CardComponent id="123" name="Placeholder Name" address="Placeholder Address" clientName="Placeholder Client Name" status="Placeholder Status" location="Placeholder Location" phone="Placeholder Phone" createdAt="Placeholder Date" recentCommission="Placeholder Commission" />
+                {cards == null ? <p>Loading...</p> : cards.map((client, index) => (<ClientCard key={index} name={client.Name} address={client.ContactAddress} phone={client.Phone} recentCommission={client.RecentCommission}></ClientCard>))}         
             </div>
         </div>
     );
+
+    async function GetClientData() {
+        const response = await fetch('https://localhost:7007/api/client');
+        console.log(response);
+        const data = await response.json();
+        console.log(data);
+        setCards(data);
+    }
 }
 
 export default CardList;
