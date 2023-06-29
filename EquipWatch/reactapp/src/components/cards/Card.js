23f122c94e34275a32757cdad6b06c03c67a58bd
Ostin-Oskarose <oskarcraft@gmail.com>
2023-06-29 11:26:13 +0200

    Implemented universal card

--- EquipWatch/reactapp/src/components/cards/Card.js
+++ EquipWatch/reactapp/src/components/cards/Card.js
@@ -1,29 +1,19 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
 import { Card } from 'react-bootstrap';
 import './Card.css';
-import { Link } from 'react-router-dom';
 
-function ClientCard({ name, address, phone, recentCommission }) {
+function ClientCard({ data }) {
     return (
         <>
             <Card className="card">
-                <div className='cardColumn'>
-                    <Card.Title className='card-title'>Client Name</Card.Title>
-                    <Card.Text>{name}</Card.Text>
-                </div>
-                <div className='cardColumn'>
-                    <Card.Title className='card-title'>Contact Address</Card.Title>
-                    <Card.Text>{address}</Card.Text>
-                </div>
-                <div className='cardColumn'>
-                    <Card.Title className='card-title'>Phone</Card.Title>
-                    <Card.Text>{phone}</Card.Text>
-                </div>
-                <div className='cardColumn'>
-                    <Card.Title className='card-title'>Most Recent Commission</Card.Title>
-                    <Card.Text>{recentCommission}</Card.Text>
-                </div>
+                {
+                    Object.keys(data).map((key, index) => (
+                        <div className='cardColumn' key={ index }>
+                            <Card.Title className='card-title'>{ key }</Card.Title>
+                            <Card.Text>{ data[key] }</Card.Text>
+                        </div>
+                    ))
+                }
             </Card>
         </>
     );
