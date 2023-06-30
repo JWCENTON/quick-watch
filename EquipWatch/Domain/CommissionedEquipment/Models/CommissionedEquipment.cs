3a6f19e09669b4339092fd6f5a3ffabcf0b368e5
Marcin Stodolny <marcin.stodolny@o2.pl>
2023-06-30 09:58:52 +0200

    renamed booking #2 unsaved change

--- EquipWatch/Domain/CommissionedEquipment/Models/CommissionedEquipment.cs
+++ EquipWatch/Domain/CommissionedEquipment/Models/CommissionedEquipment.cs
@@ -1,6 +1,6 @@
-﻿namespace Domain.BookedEquipment.Models;
+﻿namespace Domain.CommissionedEquipment.Models;
 
-public class BookedEquipment
+public class CommissionedEquipment
 {
     public Equipment.Models.Equipment Equipment { get; set; }
     public Commission.Models.Commission.Commission Booking { get; set; }
