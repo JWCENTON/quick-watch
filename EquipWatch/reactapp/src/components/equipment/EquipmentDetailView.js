68fe40e14f0dea3737ec5d3b4cdc36993c7ff9b4
dapie346 <dawid.pietruszewski@gmail.com>
2023-06-30 09:13:04 +0200

    Changed client detail and equipment detail

--- EquipWatch/reactapp/src/components/equipment/EquipmentDetailView.js
+++ EquipWatch/reactapp/src/components/equipment/EquipmentDetailView.js
@@ -1,10 +1,33 @@
+import React from 'react';
+import UniversalCard from '../card/Card';
+
+//const [Commissions, setCommissions] = useState(null);
+
+//async function GetEquipmentCommissions() {
+//    const response = await fetch('https://localhost:7007/api/client/commissions');
+//    const data = await response.json();
+//    setCommissions(data);
+//}
+
 export default function EquipmentDetailView({ detailsData }) {
     return (
-        <div>
+        <div >
             {detailsData === null ? (
                 <p>Loading...</p>
             ) : (
                 <div>
+                    <h2>Client Details</h2>
+                    <p>Serial number: {detailsData.serialNumber}</p>
+                    {/*<p>Equipment name: </p>*/}
+                    <p>Location: {detailsData.location}</p>
+                    <p>Condition: {detailsData.condition}</p>
+                    {/*<p>Status: </p>*/}
+                    <h3>Assigned Commissions</h3>
+                    {/*<div className="cardsContainer">*/}
+                    {/*    {Commissions == null ? <p>Loading...</p> : Commissions.map((card, index) => (<UniversalCard key={index} data={Commissions} dataType={'commission'}></UniversalCard>))}*/}
+                    {/*</div>*/}
+                    <button>Edit</button>
+                    <button>Remove</button>
                 </div>
             )}
         </div>
