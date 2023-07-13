3a98c04b4b8697330b5ab59abc6f22b4026e5632
Jacek Walaszek <jwalaszek@centonit.com>
2023-07-13 11:08:41 +0200

    Add email service

--- EquipWatch/webapi/Program.cs
+++ EquipWatch/webapi/Program.cs
@@ -9,7 +9,7 @@ using Microsoft.IdentityModel.Tokens;
 using Serilog;
 using webapi.Extensions;
 using webapi.Middleware;
-
+using System.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DatabaseContextConnection") ?? throw new InvalidOperationException("Connection string 'DatabaseContextConnection' not found.");
@@ -22,6 +22,8 @@ var configuration = new ConfigurationBuilder()
     .SetBasePath(builder.Environment.ContentRootPath)
     .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
     .Build();
+
+builder.Services.Configure<EmailContext>(configuration.GetSection("Email"));
 
 // Set up Serilog logger
 Log.Logger = new LoggerConfiguration()
