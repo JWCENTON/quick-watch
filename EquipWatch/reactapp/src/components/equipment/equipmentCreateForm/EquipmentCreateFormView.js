c8e1349b5e8e4e89ccaf031fb6a0debfb5954429
Ostin-Oskarose <oskarcraft@gmail.com>
2023-07-13 10:27:34 +0200

    Added post method

--- EquipWatch/reactapp/src/components/equipment/equipmentCreateForm/EquipmentCreateFormView.js
+++ EquipWatch/reactapp/src/components/equipment/equipmentCreateForm/EquipmentCreateFormView.js
@@ -2,38 +2,61 @@ import React from 'react';
 import { Rating } from '@mui/material';
 import { useState } from 'react';
 import { Button } from 'react-bootstrap';
+import { useNavigate } from "react-router-dom";
 import './equipmentCreateForm.css';
 
 export default function EquipmentCreateFormView() {
-    const [rating, setRating] = useState(0);
+    const navigate = useNavigate();
+    const [condition, setCondition] = useState(0);
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+        console.log("Form Submitted");
+        let formSerialNumber = event.target.serialNumber.value;
+        let formCategory = event.target.category.value;
+        let formLocation = event.target.category.value;
+        let formCondition = event.target.condition.value;
+
+        const response = await fetch('https://localhost:7007/api/equipment', {
+            method: "POST", body: {
+                "serialNumber": formSerialNumber,
+                "category": formCategory,
+                "location": formLocation,
+                "condition": formCondition,
+                "company": {
+                    "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+                }
+            } });
+        navigate("/equipment");
+    }
 
     return (
         <div >
-            <form>
-                <label>Serial Number: </label>
+            <form onSubmit={ handleSubmit }>
+                <label for="serialNumber">Serial Number: </label>
                 <br/>
-                <input type="text" id="serialNumber"/>
+                <input type="text" id="serialNumber" name="serialNumber" />
                 <br/>
-                <label>Category: </label>
+                <label for="category">Category: </label>
                 <br/>
-                <input type="text" id="category"/>
+                <input type="text" id="category" name="category" />
                 <br/>
-                <label>Location: </label>
+                <label for="location">Location: </label>
                 <br/>
-                <input type="text" id="location"/>
+                <input type="text" id="location" name="location" />
                 <br />
-                <label>Condition: </label>
+                <label for="rating">Condition: </label>
                 <br/>
                 <Rating
-                    id="rating"
-                    name="simple-controlled"
-                    value={rating}
+                    id="condition"
+                    name="condition"
+                    value={condition}
                     onChange={(event, newValue) => {
-                        setRating(newValue);
+                        setCondition(newValue);
                     }}
                 />
                 <br/>
-                <Button>Submit</Button>
+                <Button type="submit">Submit</Button>
             </form>
         </div>
     );
