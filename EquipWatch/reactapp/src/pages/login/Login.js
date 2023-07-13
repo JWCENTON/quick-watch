4f73009e2e96fe097e676b7dcbffd51ebf7a36cd
Jacek Walaszek <jwalaszek@centonit.com>
2023-07-13 14:01:49 +0200

    Add email not confirmed message when login

--- EquipWatch/reactapp/src/pages/login/Login.js
+++ EquipWatch/reactapp/src/pages/login/Login.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Login.css';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
 import placeholderImage from '../../images/placeholder.png';
 
@@ -8,6 +8,37 @@ function Login() {
     const location = useLocation();
     const registrationSuccess = location.state?.registrationSuccess;
     const username = location.state?.username;
+    const navigate = useNavigate();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+
+        const response = await fetch('https://localhost:7007/api/User/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                email,
+                password,
+            }),
+        });
+
+        if (response.ok) {
+            // User login successful
+            navigate('/employee');
+        } else {
+            const errorData = await response.json();
+            if (errorData.title === 'EmailNotConfirmed') {
+                setErrorMessage('Email not confirmed. Please check your email and confirm your account.');
+            } else {
+                setErrorMessage('Invalid email or password');
+            }
+        }
+    };
 
     return (
         <div className="login-page">
@@ -17,10 +48,11 @@ function Login() {
             <div className="login-right">
                 <h2>Welcome to EquipWatch</h2>
                 {registrationSuccess && <p className="success-message">User: {username} has been successfully registered</p>}
-                <form>
-                    <input type="email" placeholder="Email" />
-                    <input type="password" placeholder="Password" />
-                    <Button as={Link} to="/employee" variant="outline-primary">Login</Button>
+                {errorMessage && <p className="error-message">{errorMessage}</p>}
+                <form onSubmit={handleLogin}>
+                    <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                    <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    <Button type="submit" variant="outline-primary">Login</Button>
                     <Button as={Link} to="/register" variant="outline-primary">Register</Button>
                 </form>
             </div>
