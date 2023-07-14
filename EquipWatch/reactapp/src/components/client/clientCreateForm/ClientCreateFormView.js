3a0392f6b6535faa485deadbe29d676c6ad9891c
Ostin-Oskarose <oskarcraft@gmail.com>
2023-07-14 10:19:12 +0200

    Implemented client create form

--- EquipWatch/reactapp/src/components/client/clientCreateForm/ClientCreateFormView.js
+++ EquipWatch/reactapp/src/components/client/clientCreateForm/ClientCreateFormView.js
@@ -7,59 +7,58 @@ import './clientCreateForm.css';
 
 export default function ClientCreateFormView() {
     const navigate = useNavigate();
-    const [rating, setRating] = useState(0);
 
     async function handleSubmit(event) {
         event.preventDefault();
-        let formSerialNumber = event.target.serialNumber.value;
-        let formCategory = event.target.category.value;
-        let formLocation = event.target.category.value;
+        let formFirstName = event.target.firstName.value;
+        let formLastName = event.target.lastName.value;
+        let formEmail = event.target.email.value;
+        let formPhoneNubmer = event.target.phoneNumber.value;
+        let formAddress = event.target.address.value;
 
         let raw = JSON.stringify({
-            "serialNumber": formSerialNumber,
-            "category": formCategory,
-            "location": formLocation,
-            "condition": rating,
+            "firstName": formFirstName,
+            "lastName": formLastName,
+            "email": formEmail,
+            "phoneNumber": formPhoneNubmer,
+            "contactAddress": formAddress,
             "company": {
                 "id": "2d60c065-fae2-4b0b-87c9-08db8134c4aa"
             }
         });
 
-        const response = await fetch('https://localhost:7007/api/equipment', {
+        const response = await fetch('https://localhost:7007/api/client', {
             method: "POST", 
             headers: { "Content-Type": "application/json"},
             body: raw
         });
 
-        navigate("/equipment");
+        navigate("/clients");
     }
 
     return (
         <div >
             <form onSubmit={ handleSubmit }>
-                <label for="serialNumber">Serial Number: </label>
+                <label for="firstName">First Name: </label>
                 <br/>
-                <input type="text" id="serialNumber" name="serialNumber" />
+                <input type="text" id="firstName" name="firstName" />
                 <br/>
-                <label for="category">Category: </label>
+                <label for="lastName">Last Name: </label>
                 <br/>
-                <input type="text" id="category" name="category" />
+                <input type="text" id="lastName" name="LastName" />
                 <br/>
-                <label for="location">Location: </label>
+                <label for="email">Email: </label>
                 <br/>
-                <input type="text" id="location" name="location" />
+                <input type="email" id="email" name="email" />
                 <br />
-                <label for="rating">Condition: </label>
-                <br/>
-                <Rating
-                    id="rating"
-                    name="rating"
-                    value={rating}
-                    onChange={(event, newValue) => {
-                        setRating(newValue);
-                    }}
-                />
-                <br/>
+                <label for="phoneNumber">Phone Number: </label>
+                <br />
+                <input type="text" id="phoneNumber" name="phoneNumber" />
+                <br />
+                <label for="address">Address: </label>
+                <br />
+                <input type="text" id="address" name="address" />
+                <br />
                 <Button type="submit">Submit</Button>
             </form>
         </div>
