5e962519ce754ca33d1bd161e9ca9be734e50e28
Marcin Stodolny <marcin.stodolny@o2.pl>
2023-07-24 10:20:20 +0200

    removed isCheckedOut from UpdateEquipment DTO

--- EquipWatch/DTO/EquipmentDTOs/UpdateEquipmentDTO.cs
+++ EquipWatch/DTO/EquipmentDTOs/UpdateEquipmentDTO.cs
@@ -5,7 +5,6 @@ namespace DTO.EquipmentDTOs;
 public record UpdateEquipmentDTO
 {
     public CompanyIdDTO? Company { get; init; }
-    public bool? IsCheckedOut { get; init; }
     public string? Category { get; init; }
     public string? SerialNumber { get; init; }
     public string? Location { get; init; }
