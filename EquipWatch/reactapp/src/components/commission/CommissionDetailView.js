3c2317a075af6cee96738cf2753395c999dc6ee9
Ostin-Oskarose <oskarcraft@gmail.com>
2023-07-27 10:20:54 +0200

    Added equipment and worker, lists and modals

--- EquipWatch/reactapp/src/components/commission/CommissionDetailView.js
+++ EquipWatch/reactapp/src/components/commission/CommissionDetailView.js
@@ -1,16 +1,61 @@
+import { useState } from 'react';
+import { Button, Modal } from 'react-bootstrap';
+import UniversalCard from '../card/Card';
+import './CommissionDetailView.css';
+
 export default function CommissionDetailView({ detailsData }) {
+    const [equipment, setEquipment] = useState(null);
+    const [workers, setWorkers] = useState(null);
+    const [showEquipmentModal, setShowEquipmentModal] = useState(false);
+    const [showWorkerModal, setShowWorkerModal] = useState(false);
+
+    const handleEquipmentClose = () => setShowEquipmentModal(false);
+    const handleEquipmentShow = () => setShowEquipmentModal(true);
+
+    const handleWorkerClose = () => setShowWorkerModal(false);
+    const handleWorkerShow = () => setShowWorkerModal(true);
+
     return (
         <div>
             {detailsData === null ? (
                 <p>Loading...</p>
             ) : (
-                <div>
-                        <h2>Location:</h2>
-                        <h3>{detailsData.location}</h3>
-                        <h2>Description:</h2>
-                        <h3>{detailsData.description}</h3>
-                        <h2>Scope:</h2>
-                        <h3>{detailsData.scope}</h3>
+                    <div>
+                        <h2>Commission Details</h2>
+                        <p>Location: {detailsData.location}</p>
+                        <p>Description: {detailsData.description}</p>
+                        <p>Scope: {detailsData.scope}</p>
+                        <h3>Equipment</h3>
+                        <div className="cardsContainer">
+                            {equipment == null ? <p>Loading Equipment...</p> : equipment.map((item, index) => (<UniversalCard key={index} data={item} dataType='commission'></UniversalCard>))}
+                        </div>
+                        <Button onClick={handleEquipmentShow}>Add Equipment</Button>
+                        <h3>Workers</h3>
+                        <div className="cardsContainer">
+                            {workers == null ? <p>Loading Workers...</p> : workers.map((worker, index) => (<UniversalCard key={index} data={worker} dataType='employee'></UniversalCard>))}
+                        </div>
+                        <Button onClick={handleWorkerShow}>Add Worker</Button>
+
+                        <Modal show={showEquipmentModal} onHide={handleEquipmentClose}>
+                            <Modal.Header closeButton>
+                                <Modal.Title>Add Equipment</Modal.Title>
+                            </Modal.Header>
+                                <Modal.Body>
+                                    <label for="inlocation">Location:</label>
+                                    <br />
+                                    <input type="text" id="inlocation" name="location" />
+                                </Modal.Body>
+                        </Modal>
+                        <Modal show={showWorkerModal} onHide={handleWorkerClose}>
+                            <Modal.Header closeButton>
+                                <Modal.Title>Add Worker</Modal.Title>
+                            </Modal.Header>
+                                <Modal.Body>
+                                    <label for="inlocation">Location:</label>
+                                    <br />
+                                    <input type="text" id="inlocation" name="location" />
+                                </Modal.Body>
+                        </Modal>
                 </div>
             )}
         </div>
