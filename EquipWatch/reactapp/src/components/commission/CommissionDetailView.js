1f8746c981ce6a29619d4c668e185bcba12d870d
Ostin-Oskarose <oskarcraft@gmail.com>
2023-07-27 09:21:09 +0200

    Added basic commission view

--- EquipWatch/reactapp/src/components/commission/CommissionDetailView.js
+++ EquipWatch/reactapp/src/components/commission/CommissionDetailView.js
@@ -5,6 +5,12 @@ export default function CommissionDetailView({ detailsData }) {
                 <p>Loading...</p>
             ) : (
                 <div>
+                        <h2>Location:</h2>
+                        <h3>{detailsData.location}</h3>
+                        <h2>Description:</h2>
+                        <h3>{detailsData.description}</h3>
+                        <h2>Scope:</h2>
+                        <h3>{detailsData.scope}</h3>
                 </div>
             )}
         </div>
