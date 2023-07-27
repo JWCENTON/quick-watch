f83b7c96ea8fd98cf92c49d61ce04b37f5c59ce6
dapie346 <dawid.pietruszewski@gmail.com>
2023-07-27 09:23:21 +0200

    some styling changes

--- EquipWatch/reactapp/src/components/equipment/EquipmentDetailView.js
+++ EquipWatch/reactapp/src/components/equipment/EquipmentDetailView.js
@@ -1,16 +1,9 @@
 import React from 'react';
-import { useState, useContext } from 'react';
+import { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import { Modal, Button } from 'react-bootstrap';
 import { useAuth } from '../authProvider/AuthContext';
-
-//const [Commissions, setCommissions] = useState(null);
-
-//async function GetEquipmentCommissions() {
-//    const response = await fetch('https://localhost:7007/api/client/commissions');
-//    const data = await response.json();
-//    setCommissions(data);
-//}
+import './EquipmentDetailView.css';
 
 export default function EquipmentDetailView({ detailsData }) {
     const [showCheckoutModal, setShowCheckoutModal] = useState(false);
@@ -75,55 +68,66 @@ export default function EquipmentDetailView({ detailsData }) {
     }
 
     return (
-        <div >
+        <div className="equipment-details">
+            <div className="myAndAllSwitch"><a className="myAndAllSwitch" href="/equipment" >My Equipment</a> | <a className="myAndAllSwitch" href="/equipment" >All Equipment</a></div>
             {detailsData === null ? (
                 <p>Loading...</p>
             ) : (
-                <div>
-                    <h2>Client Details</h2>
-                    <p>Serial number: {detailsData.serialNumber}</p>
-                    {/*<p>Equipment name: </p>*/}
-                    <p>Location: {detailsData.location}</p>
-                    <p>Condition: {detailsData.condition}</p>
-                    {/*<p>Status: </p>*/}
-                    <h3>Assigned Commissions</h3>
-                    {/*<div className="cardsContainer">*/}
-                    {/*    {Commissions == null ? <p>Loading...</p> : Commissions.map((card, index) => (<UniversalCard key={index} data={Commissions} dataType={'commission'}></UniversalCard>))}*/}
-                    {/*</div>*/}
-                        <button>Edit</button>
-                        <Button onClick={handleCheckoutModalShow}>Checkout</Button>
-                        <Button onClick={handleCheckinModalShow}>Checkin</Button>
-                        <button onClick={DeleteEquipment}>Remove</button>
-                        <Modal show={showCheckoutModal} onHide={handleCheckoutModalClose}>
-                            <Modal.Header closeButton>
-                                <Modal.Title>Checkout Equipment</Modal.Title>
-                            </Modal.Header>
-                            <form onSubmit={handleCheckoutFormSubmit}>
-                                <Modal.Body>
-                                    <label for="outlocation">Location:</label>
-                                    <br />
-                                    <input type="text" id="outlocation" name="location" />
-                                </Modal.Body>
-                                <Modal.Footer>
-                                    <Button type="submit">Checkout</Button>
-                                </Modal.Footer>
-                            </form>
-                        </Modal>
-                        <Modal show={showCheckinModal} onHide={handleCheckinModalClose}>
-                            <Modal.Header closeButton>
-                                <Modal.Title>Checkin Equipment</Modal.Title>
-                            </Modal.Header>
-                            <form onSubmit={handleCheckinFormSubmit}>
-                                <Modal.Body>
-                                    <label for="inlocation">Location:</label>
-                                    <br />
-                                    <input type="text" id="inlocation" name="location" />
-                                </Modal.Body>
-                                <Modal.Footer>
-                                    <Button type="submit">Checkin</Button>
-                                </Modal.Footer>
-                            </form>
-                        </Modal>
+                    <div className="details-grid">
+                        
+                    <div className="details-section">
+                        <h2>Equipment Details</h2>
+                        <p>Serial number: {detailsData.serialNumber}</p>
+                        <p>Location: {detailsData.location}</p>
+                        <p>Condition:
+                            {[...Array(detailsData.condition)].map((e, i) => <span className="star" key={i}>&#9733;</span>)}
+                        </p>
+                    </div>
+                        <div className="commission-section">
+                        <h3>Assigned Commission</h3>
+                        <Button className="add-commission-btn">Add Commission</Button>
+                        <h3>Assigned Employee</h3>
+
+                        <Button className="checkout-btn" onClick={handleCheckoutModalShow}>Checkout</Button>
+                        <Button className="checkin-btn" onClick={handleCheckinModalShow}>Checkin</Button>
+                    </div>
+
+                    <div className="button-section">
+                        <Button className="edit-btn">Edit</Button>
+                        <Button className="remove-btn" onClick={DeleteEquipment}>Remove</Button>
+                    </div>
+
+                    <Modal show={showCheckoutModal} onHide={handleCheckoutModalClose}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>Checkout Equipment</Modal.Title>
+                        </Modal.Header>
+                        <form onSubmit={handleCheckoutFormSubmit}>
+                            <Modal.Body>
+                                <label htmlFor="outlocation">Location:</label>
+                                <br />
+                                <input type="text" id="outlocation" name="location" />
+                            </Modal.Body>
+                            <Modal.Footer>
+                                <Button type="submit">Checkout</Button>
+                            </Modal.Footer>
+                        </form>
+                    </Modal>
+
+                    <Modal show={showCheckinModal} onHide={handleCheckinModalClose}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>Checkin Equipment</Modal.Title>
+                        </Modal.Header>
+                        <form onSubmit={handleCheckinFormSubmit}>
+                            <Modal.Body>
+                                <label htmlFor="inlocation">Location:</label>
+                                <br />
+                                <input type="text" id="inlocation" name="location" />
+                            </Modal.Body>
+                            <Modal.Footer>
+                                <Button type="submit">Checkin</Button>
+                            </Modal.Footer>
+                        </form>
+                    </Modal>
                 </div>
             )}
         </div>
