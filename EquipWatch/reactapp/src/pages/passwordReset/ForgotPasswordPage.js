060bbd4a50fb1f8c9a3eb2cf54162ef00468ec44
Jacek Walaszek <jwalaszek@centonit.com>
2023-08-11 11:35:41 +0200

    Create and Add login layout for registration page and password reset

--- EquipWatch/reactapp/src/pages/passwordReset/ForgotPasswordPage.js
+++ EquipWatch/reactapp/src/pages/passwordReset/ForgotPasswordPage.js
@@ -1,19 +1,9 @@
-import './ForgotPassword.css';
-import placeholderImage from '../../images/placeholder.png';
-import ForgotPassword from '../../components/forgotPassword/ForgotPassword'
+import ForgotPassword from '../../components/forgotPassword/ForgotPassword';
+import LoginLayout from '../../components/layout/LoginLayout';
 
 function ForgotPasswordPage() {
-
     return (
-        <div className="login-page">
-            <div className="login-left">
-                <img className="login-image" src={placeholderImage} alt="Placeholder" />
-            </div>
-            <div className="login-right">
-                <h2>Welcome to EquipWatch</h2>
-                <ForgotPassword />
-            </div>
-        </div>
+        <LoginLayout children={<ForgotPassword />} />                
     );
 }
 
--- EquipWatch/reactapp/src/pages/registration/Registration.js
+++ EquipWatch/reactapp/src/pages/registration/Registration.js
@@ -1,18 +1,9 @@
-import './Registration.css';
-import placeholderImage from '../../images/placeholder.png';
-import UserRegistration from '../../components/userRegistration/UserRegistration'
-function Registration() {
+import UserRegistration from '../../components/userRegistration/UserRegistration';
+import LoginLayout from '../../components/layout/LoginLayout';
 
+function Registration() {
     return (
-        <div className="login-page">
-            <div className="login-left">
-                <img className="login-image" src={placeholderImage} alt="Placeholder" />
-            </div>
-            <div className="login-right">
-                <h2>Welcome to EquipWatch</h2>
-                <UserRegistration />
-            </div>
-        </div>
+        <LoginLayout children={<UserRegistration />} />
     );
 }
 
