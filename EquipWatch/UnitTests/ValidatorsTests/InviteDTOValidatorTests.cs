0ce90a3e62e0a66fdd6f0958dcc2db9e275a92be
dapie346 <dawid.pietruszewski@gmail.com>
2023-08-21 13:14:56 +0200

    some tests for InviteDTOValidator and UserDTOValidator

---
+++ EquipWatch/UnitTests/ValidatorsTests/InviteDTOValidatorTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using webapi.Validators;
+using DTO.InviteDTOs;
+using System;
+using Domain.Invite;
+
+namespace EquipWatch.UnitTests.ValidatorsTests
+{
+    public class InviteDTOValidatorTests
+    {
+        [Test]
+        public void FullValidator_ShouldFail_WhenCreatedAtIsInFuture()
+        {
+            var validator = new FullInviteDTOValidator();
+            var dto = new FullInviteDTO
+            {
+                Id = Guid.NewGuid().ToString(),
+                CompanyId = Guid.NewGuid().ToString(),
+                UserId = Guid.NewGuid().ToString(),
+                Status = Status.Sent,
+                CreatedAt = DateTime.UtcNow.AddMinutes(1)
+            };
+
+            var result = validator.Validate(dto);
+
+            Assert.IsFalse(result.IsValid);
+            Assert.AreEqual("Created at date cannot be in the future.", result.Errors[0].ErrorMessage);
+        }
+
+        [Test]
+        public void UpdateValidator_ShouldFail_WhenStatusIsInvalid()
+        {
+            var validator = new UpdateInviteDTOValidator();
+            var dto = new UpdateInviteDTO
+            {
+                Status = (Status)999
+            };
+
+            var result = validator.Validate(dto);
+
+            Assert.IsFalse(result.IsValid);
+            Assert.AreEqual($"Invalid status. Status must be one of the following values: {string.Join(", ", Enum.GetNames(typeof(Status)))}", result.Errors[0].ErrorMessage);
+        }
+    }
+}
---
+++ EquipWatch/UnitTests/ValidatorsTests/UserDTOValidatorTests.cs
@@ -0,0 +1,76 @@
+﻿using NUnit.Framework;
+using webapi.Validators;
+using DTO.UserDTOs;
+
+namespace EquipWatch.UnitTests.ValidatorsTests
+{
+    public class UserDTOValidatorTests
+    {
+        [Test]
+        public void FullUserValidator_ShouldFail_WhenIdIsInvalid()
+        {
+            var validator = new FullUserDTOValidator();
+            var dto = new FullUserDTO
+            {
+                Id = "InvalidId",
+                Email = "test@example.com",
+                Password = "Password1!",
+                UserName = "UserName"
+            };
+
+            var result = validator.Validate(dto);
+
+            Assert.IsFalse(result.IsValid);
+            Assert.AreEqual("Invalid User ID format.", result.Errors[0].ErrorMessage);
+        }
+
+        [Test]
+        public void CreateUserValidator_ShouldFail_WhenFirstNameIsEmpty()
+        {
+            var validator = new CreateUserDTOValidator();
+            var dto = new CreateUserDTO
+            {
+                FirstName = "",
+                LastName = "LastName",
+                Email = "test@example.com",
+                Password = "Password1!"
+            };
+
+            var result = validator.Validate(dto);
+
+            Assert.IsFalse(result.IsValid);
+            Assert.AreEqual("First name cannot be empty.", result.Errors[0].ErrorMessage);
+        }
+
+        [Test]
+        public void LoginUserValidator_ShouldPass_WhenAllFieldsAreValid()
+        {
+            var validator = new LoginUserDTOValidator();
+            var dto = new LoginUserDTO
+            {
+                Email = "test@example.com",
+                Password = "Password1!"
+            };
+
+            var result = validator.Validate(dto);
+
+            Assert.IsTrue(result.IsValid);
+        }
+
+        [Test]
+        public void UpdateUserValidator_ShouldFail_WhenEmailIsInvalid()
+        {
+            var validator = new UpdateUserDTOValidator();
+            var dto = new UpdateUserDTO
+            {
+                Email = "invalid-email",
+                UserName = "UserName"
+            };
+
+            var result = validator.Validate(dto);
+
+            Assert.IsFalse(result.IsValid);
+            Assert.AreEqual("Email must contain @.", result.Errors[0].ErrorMessage);
+        }
+    }
+}
