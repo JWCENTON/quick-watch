7f8ee890d70fe0ed9f2fb96e5925784455086111
Ostin-Oskarose <oskarcraft@gmail.com>
2023-08-21 10:09:59 +0200

    QR code is now downloadable

--- EquipWatch/reactapp/src/components/equipment/EquipmentDetailView.js
+++ EquipWatch/reactapp/src/components/equipment/EquipmentDetailView.js
@@ -99,6 +99,26 @@ export default function EquipmentDetailView({ detailsData }) {
         navigate("/equipment");
     }
 
+    function onImageDownload()
+    {
+        const svg = document.getElementById("QRCode");
+        const svgData = new XMLSerializer().serializeToString(svg);
+        const canvas = document.createElement("canvas");
+        const ctx = canvas.getContext("2d");
+        const img = new Image();
+        img.onload = () => {
+            canvas.width = img.width;
+            canvas.height = img.height;
+            ctx.drawImage(img, 0, 0);
+            const pngFile = canvas.toDataURL("image/png");
+            const downloadLink = document.createElement("a");
+            downloadLink.download = `${detailsData.serialNumber} QRCode`;
+            downloadLink.href = `${pngFile}`;
+            downloadLink.click();
+        };
+        img.src = `data:image/svg+xml;base64,${btoa(svgData)}`;
+    };
+
     return (
         <div className="details-section">
             <div className="myAndAllSwitch-section"><a className="myAndAllSwitch" href="/equipment" >My Equipment</a> | <a className="myAndAllSwitch" href="/equipment" >All Equipment</a></div>
@@ -120,7 +140,8 @@ export default function EquipmentDetailView({ detailsData }) {
                             <Button className="detail-view-btn" onClick={DeleteEquipment}>Remove</Button>
                             </div>
                             <h4 className="details-header">QR Code</h4>
-                            <QRCode value={window.location.href}></QRCode>
+                            <QRCode id="QRCode" value={window.location.href}></QRCode>
+                            <Button className="detail-view-btn" onClick={onImageDownload}>Download QR Code</Button>
                     </div>
                     <div className="section-right">
                         <h4 className="details-header">Assigned Commission</h4>
