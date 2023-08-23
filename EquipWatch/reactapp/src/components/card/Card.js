6ce94b0a8399d62307836876cf8da8d3f4a5a8ba
dapie346 <dawid.pietruszewski@gmail.com>
2023-08-23 13:30:39 +0200

    Cards are now displayed in grids

--- EquipWatch/reactapp/src/components/card/Card.js
+++ EquipWatch/reactapp/src/components/card/Card.js
@@ -5,6 +5,11 @@ import './Card.css';
 
 function UniversalCard({ data, dataType }) {
     const filteredData = Object.entries(data).filter(([key]) => key !== 'id');
+    let numberOfItems = filteredData.length;
+
+    if (numberOfItems > 4) {
+        numberOfItems = Math.ceil(numberOfItems / 2);
+    }
 
     function camelCaseToSentence(str) {
         return str.replace(/([A-Z])/g, ' $1').split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(' ');
@@ -12,11 +17,11 @@ function UniversalCard({ data, dataType }) {
 
     return (
         <Link to={`/${dataType}/${data.id}`}>
-            <Card className="card">
+            <Card className="card" style={{ gridTemplateColumns: `repeat(${numberOfItems}, 1fr)` }}>
                 {filteredData.map(([key, value]) => (
                     <div className='cardColumn' key={key}>
                         <Card.Title className='card-title'>{camelCaseToSentence(key)}</Card.Title>
-                        <Card.Text>{value}</Card.Text>
+                        <Card.Text className='card-text'>{value}</Card.Text>
                     </div>
                 ))}
             </Card>
