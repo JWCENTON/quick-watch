9053017e6e8ef1c705cb0bb051d3a2fbb6a40513
dapie346 <dawid.pietruszewski@gmail.com>
2023-08-23 12:34:01 +0200

    camel case headers in cards are being transformed to regular headers now

--- EquipWatch/reactapp/src/components/card/Card.js
+++ EquipWatch/reactapp/src/components/card/Card.js
@@ -4,16 +4,21 @@ import { Link } from 'react-router-dom';
 import './Card.css';
 
 function UniversalCard({ data, dataType }) {
-	 const filteredData = Object.entries(data).filter(([key]) => key !== 'id');
+    const filteredData = Object.entries(data).filter(([key]) => key !== 'id');
+
+    function camelCaseToSentence(str) {
+        return str.replace(/([A-Z])/g, ' $1').split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(' ');
+    }
+
     return (
         <Link to={`/${dataType}/${data.id}`}>
             <Card className="card">
                 {filteredData.map(([key, value]) => (
-				<div className='cardColumn' key={key}>
-				<Card.Title className='card-title'>{key}</Card.Title>
-				<Card.Text>{value}</Card.Text>
-          </div>
-        ))}
+                    <div className='cardColumn' key={key}>
+                        <Card.Title className='card-title'>{camelCaseToSentence(key)}</Card.Title>
+                        <Card.Text>{value}</Card.Text>
+                    </div>
+                ))}
             </Card>
         </Link>
     );
